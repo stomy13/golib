dd08fd2e136d612d3b610139ebf46215143b0193
stomy13 <48572568+stomy13@users.noreply.github.com>
2025-02-23 22:47:47 +0900

    connect-webプロジェクトの初期設定を追加し、Reactアプリケーションの基本構造を構築

---
+++ api/connect-web/src/App.tsx
@@ -0,0 +1,67 @@
+import { useState } from 'react'
+import './App.css'
+import { createClient } from "@connectrpc/connect";
+import { createConnectTransport } from "@connectrpc/connect-web";
+
+// Import service definition that you want to connect to.
+import { ElizaService } from "@buf/connectrpc_eliza.bufbuild_es/connectrpc/eliza/v1/eliza_pb";
+
+// The transport defines what type of endpoint we're hitting.
+// In our example we'll be communicating with a Connect endpoint.
+// If your endpoint only supports gRPC-web, make sure to use
+// `createGrpcWebTransport` instead.
+const transport = createConnectTransport({
+  baseUrl: "https://demo.connectrpc.com",
+});
+
+// Here we make the client itself, combining the service
+// definition with the transport.
+const client = createClient(ElizaService, transport);
+
+function App() {
+  const [inputValue, setInputValue] = useState("");
+  const [messages, setMessages] = useState<
+    {
+      fromMe: boolean;
+      message: string;
+    }[]
+  >([]);
+  return <>
+      <ol>
+      {messages.map((msg, index) => (
+        <li key={index}>
+          {`${msg.fromMe ? "ME:" : "ELIZA:"} ${msg.message}`}
+        </li>
+      ))}
+    </ol>
+    <form onSubmit={async (e) => {
+      e.preventDefault();
+      // Clear inputValue since the user has submitted.
+      setInputValue("");
+      // Store the inputValue in the chain of messages and
+      // mark this message as coming from "me"
+      setMessages((prev) => [
+        ...prev,
+        {
+          fromMe: true,
+          message: inputValue,
+        },
+      ]);
+      const response = await client.say({
+        sentence: inputValue,
+      });
+      setMessages((prev) => [
+        ...prev,
+        {
+          fromMe: false,
+          message: response.sentence,
+        },
+      ]);
+    }}>
+      <input value={inputValue} onChange={e => setInputValue(e.target.value)} />
+      <button type="submit">Send</button>
+    </form>
+    </>
+}
+
+export default App
